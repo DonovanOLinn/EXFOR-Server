5d7894bf113ebc2e840b467e2c9d9126655acafb
DonovanOLinn <donovan.b.olinn@gmail.com>
2024-11-05 17:20:47 -0600

    adding in webscraper for books

--- scripts/exfor-book-scripts.sql
+++ scripts/exfor-book-scripts.sql
@@ -1,6 +1,6 @@
 select * from books;
 
-ALTER TABLE books MODIFY COLUMN author_summary VARCHAR(2000);
+ALTER TABLE books MODIFY COLUMN author_summary VARCHAR(5000);
     
 INSERT INTO books (book_id, book_name, release_date, previous, next, author_summary, image, author, narrator, run_time)
 VALUES (1, "Columbus Day", "2016-12-13", "None", "Spec Ops", "We were fighting on the wrong side, of a war we couldn't win. And that was the good news.The Ruhar hit us on Columbus Day. 
@@ -15,7 +15,26 @@ He doesn't agree when the UN decides to send almost 70 elite Special Operations 
 At least this time, the Earth is safe, right? Not so much.", "https://static.wikia.nocookie.net/expeditionary-force-by-craig-alanson/images/f/f7/ExForce_Book_2_Spec_Ops_2.jpg/revision/latest?cb=20201029203035", 
 "Craig Alanson", "R.C. Bray", "15 hours 49 minutes");
 INSERT INTO books (book_id, book_name, release_date, previous, next, author_summary, image, author, narrator, run_time)
-VALUES (3, "Paradise", "2017-03-07", "Spec Ops", "While the crew of the starship Flying Dutchman have been trying to assure that hostile aliens do not have access to Earth, the UN Expeditionary Force has been stranded
+VALUES (3, "Paradise", "2017-03-07", "Spec Ops", "Trouble on Paradise (Novella)", "While the crew of the starship Flying Dutchman have been trying to assure that hostile aliens do not have access to Earth, the UN Expeditionary Force has been stranded
  on the planet they nicknamed 'Paradise'. The Flying Dutchman is headed back out on another mission, and the UN wants the ship to find out the status of the humans on Paradise. But Colonel Joe Bishop warns 
  that they might not like what they find, and they can't do anything about it without endangering Earth.", 
  "https://static.wikia.nocookie.net/expeditionary-force-by-craig-alanson/images/8/8b/ExForce_Book_3_Paradise_2.jpg/revision/latest?cb=20201029203055", "Craig Alanson", "R.C. Bray", "15 hours 53 minutes");
+ 
+ select * from characters;
+ INSERT INTO characters (character_id, character_name, description, status, last_known_location, sex, first_book_appearance_id)
+ VALUES (1, "Joseph Bishop", "Joseph Arthur Bishop is an Army soldier from Maine, who finds himself in a whole heap of adventure due to no fault of his own. Joe is also the main character of the series 
+ and the narrator of most parts of the books in the main series.", "alive", "valkyrie", "male", 1);
+ 
+select * from planets;
+INSERT INTO planets (planet_id, planet_name, planet_nickname, first_book_appearance_id)
+VALUES(1, "Earch", "Home", 1);
+
+UPDATE planets SET planet_name = "Earth" WHERE planet_id = 1;
+
+select * from species;
+INSERT INTO species (species_id, species_name, appearance, patron, tech_level, nickname, coalition)
+VALUES (1, "Humans", "Roughly 2 meters tall, bi-pedal, with varying skin tone", "None", "Tier 1", "Monkeys", "Human Coalition");
+
+select * from ships;
+INSERT INTO ships (ship_id, ship_name, ship_type, status, species_id)
+VALUES (1, "The Flying Dutchman V4", "Warship", "breaking apart in atmosphere", 1);
