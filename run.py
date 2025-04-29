c5df8d6408c100dc6c7321250f893c2936282a09
DonovanOLinn <donovan.b.olinn@gmail.com>
2025-04-28 20:28:31 -0500

    Added in CORS to connect with frontend

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from app import create_app
 from dotenv import load_dotenv
 from tasks.scraper_tasks import run_all_scrapers
 from app.models import db
+
+
 load_dotenv()
 
 app = create_app()
@@ -10,5 +12,5 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        run_all_scrapers()
+        # run_all_scrapers()
     app.run(debug=True)
