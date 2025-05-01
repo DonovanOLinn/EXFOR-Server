d735b6e56f3e52d7372b8b01d038d34fbfc4c67e
DonovanOLinn <donovan.b.olinn@gmail.com>
2025-05-01 08:12:04 -0500

    Implemented by id & by name routes for character, planet, ships, species

--- app/routes/character.py
+++ app/routes/character.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from sqlalchemy import select
 from app.models import db
-from app.models.characters import Characters, characters_schema
+from app.models.characters import Characters, characters_schema, character_schema
 
 characters_bp = Blueprint("characters", __name__)
 
@@ -12,3 +12,17 @@ def get_characters():
     result = db.session.execute(rows).scalars()
     characters = result.all()
     return characters_schema.dump(characters)
+
+@characters_bp.route("/characters/<int:character_id>", methods=['GET'])
+def get_character_by_id(character_id):
+    row = select(Characters).where(Characters.character_id == character_id)
+    result = db.session.execute(row).scalar_one_or_none()
+    character = result
+    return character_schema.dump(character)
+
+@characters_bp.route("/characters/<string:character_name>", methods=['GET'])
+def get_character_by_name(character_name):
+    row = select(Characters).where(Characters.character_name == character_name)
+    result = db.session.execute(row).scalar_one_or_none()
+    character = result
+    return character_schema.dump(character)
--- app/routes/planet.py
+++ app/routes/planet.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from sqlalchemy import select
 from app.models import db
-from app.models.planets import Planets, planets_schema
+from app.models.planets import Planets, planets_schema, planet_schema
 
 planets_bp = Blueprint("planets", __name__)
 
@@ -12,3 +12,17 @@ def get_planets():
     result = db.session.execute(rows).scalars()
     planets = result.all()
     return planets_schema.dump(planets)
+
+@planets_bp.route("/planets/<int:planet_id>", methods=['GET'])
+def get_character_by_id(planet_id):
+    row = select(Planets).where(Planets.planet_id == planet_id)
+    result = db.session.execute(row).scalar_one_or_none()
+    # character = result
+    return planet_schema.dump(result)
+
+@planets_bp.route("/planets/<string:planet_name>", methods=['GET'])
+def get_character_by_name(planet_name):
+    row = select(Planets).where(Planets.planet_name == planet_name)
+    result = db.session.execute(row).scalar_one_or_none()
+    # character = result
+    return planet_schema.dump(result)
